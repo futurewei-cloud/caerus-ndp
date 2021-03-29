c498ad6b31271998e7a7019c35820f1f451198d3
Peter Puhov <peterpuhov.github@gmail.com>
2021-03-29 09:40:08 -0400

    docker/entry.sh added default docker mode

--- hadoop/docker/entry.sh
+++ hadoop/docker/entry.sh
@@ -12,10 +12,11 @@ trap _term SIGTERM
 echo "PID $BASHPID SIGTERM trap enabled"
 echo "$@"
 
-if [ $RUNNING_MODE = "daemon" ]; then
+if [ "${RUNNING_MODE:-RUNNING_MODE_DEFAULT}" = "daemon" ]; then
     eval "$@" &
     child=$! 
     wait "$child"
+    exit
 fi
 
 eval "$@"
