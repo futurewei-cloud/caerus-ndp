f190ea950813226a3f2b93e5bc8713ab82acb00e
Robert Foley <robfoley972@gmail.com>
2021-04-01 16:24:32 -0400

    Update scripts for debug jars.

--- benchmark/test.sh
+++ benchmark/test.sh
@@ -40,13 +40,13 @@ if [ ${DEBUG} == "YES" ]; then
   --conf "spark.jars.ivy=/build/ivy" \
   --conf "spark.driver.extraJavaOptions=-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=172.18.0.2:5005" \
   --packages com.github.scopt:scopt_2.12:4.0.0-RC2,com.typesafe:config:1.4.1 \
-  --jars /datasource/lib/ndp-hdfs-1.0.jar,/benchmark/spark-sql-perf/target/scala-2.12/spark-sql-perf_2.12-0.5.1-SNAPSHOT.jar,/datasource/target/scala-2.12/ndp-datasource_2.12-0.1.0.jar \
+  --jars /datasource/datasource/lib/ndp-hdfs-1.0.jar,/benchmark/spark-sql-perf/target/scala-2.12/spark-sql-perf_2.12-0.5.1-SNAPSHOT.jar,/datasource/datasource/target/scala-2.12/ndp-datasource_2.12-0.1.0.jar \
    /benchmark/perf-test/target/scala-2.12/perf-test_2.12-1.0.jar $@
 else
   docker exec -it sparkmaster spark-submit --master local[$WORKERS] \
   --class com.github.perf.${TEST} \
   --packages com.github.scopt:scopt_2.12:4.0.0-RC2,com.typesafe:config:1.4.1 \
-  --jars /datasource/lib/ndp-hdfs-1.0.jar,/benchmark/spark-sql-perf/target/scala-2.12/spark-sql-perf_2.12-0.5.1-SNAPSHOT.jar,/datasource/target/scala-2.12/ndp-datasource_2.12-0.1.0.jar \
+  --jars /datasource/datasource/lib/ndp-hdfs-1.0.jar,/benchmark/spark-sql-perf/target/scala-2.12/spark-sql-perf_2.12-0.5.1-SNAPSHOT.jar,/datasource/datasource/target/scala-2.12/ndp-datasource_2.12-0.1.0.jar \
    /benchmark/perf-test/target/scala-2.12/perf-test_2.12-1.0.jar $@
 fi
 # Add the below to the lines above to override the hostname.
